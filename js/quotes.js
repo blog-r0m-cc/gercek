22cb5f9054a999d4f5679b7c30291afcef4f292d
blog-r0m-cc <blog@r0m.cc>
2024-09-17 21:11:50 +0300

    update

---
+++ js/quotes.js
@@ -0,0 +1,62 @@
+const RANDOM_QUOTES = [
+	{
+		text: "Bazen doğruyu görebilmek için yanlışa düşmek gerekir.",
+		author: "Ege Bilecen"
+	},
+	{
+		text: "Başkasının sözünün yolcusu olmayın, bütün sözleri kendi akıl süzgecinizden geçirin.",
+		author: "Ege Bilecen"
+	},
+	{
+		text: "Onlar ki, sözü dinler ve en güzeline uyarlar. İşte onlar Allah'ın doğru yola ulaştırdığı kişilerdir. Öz akıl sahipleri de işte sadece onlardır.",
+		author: "Kur'an, 39:18"
+	},
+	{
+		text: "Hakkında bilgi sahibi olmadığın şeyin peşine düşme! Kuşkusuz kulak, göz ve beyin bunların hepsi ondan sorumludur.",
+		author: "Kur'an, 17:36"
+	},
+	{
+		text: "Ant olsun biz, Kur'an'ı öğüt alınsın diye kolaylaştırdık. Var mı öğüt alıp düşünen?",
+		author: "Kur'an, 54:17"
+	},
+	{
+		text: "İyilikle kötülük bir değildir. Kötülüğü iyilikle sav. Bir de bakmışsın ki seninle arasında düşmanlık olan kişi, en iyi dostun oluverir.",
+		author: "Kur'an, 41:34"
+	},
+	{
+		text: "De ki: \"İster Allah diye çağırın ister Rahman diye çağırın. Hangisiyle çağırırsanız çağırın en iyi isimler O'nundur.\" (…)",
+		author: "Kur'an, 17:110"
+	},
+	{
+		text: "Kim bir kötülük yaparsa veya kendisine haksızlık eder de sonra Allah'tan bağışlanma dilerse, Allah'ı çok bağışlayıcı, çok merhametli bulur",
+		author: "Kur'an, 4:110"
+	},
+	{
+		text: "Ondan (Kur’an’dan) yüz çeviren kişi, kıyamet günü içinde ebedî kalacakları ağır bir günah yükünü yüklenecektir. Bu, onlar için kıyamet gününde ne kötü bir yüktür!",
+		author: "Kur'an, 20:100-101"
+	},
+	{
+		text: "İnsan hayra dua eder gibi, şerre dua ediyor. İnsan çok acelecidir.",
+		author: "Kur'an, 17:11"
+	},
+	{
+		text: "İnsana bir zarar dokunduğu zaman bize yalvarır. Sonra, kendisine tarafımızdan bir nimet verdiğimizde \"Bu, bana ancak bilgim sayesinde verilmiştir\" der. Hayır! O bir imtihandır fakat çoğu bilmez.",
+		author: "Kur'an, 39:49"
+	},
+	{
+		text: "İnsana bir sıkıntı dokunduğu zaman, yan yatarak, oturarak veya ayakta durarak bize dua eder. Fakat ondan sıkıntısını kaldırınca, sanki kendisine dokunan bir sıkıntı yüzünden bize yalvarmamış gibi geçip gider. İşte böylece yapmakta oldukları şeyler haddi aşanlara güzel gösterilmiştir.",
+		author: "Kur'an, 10:12"
+	},
+	{
+		text: "İnsana tarafımızdan bir rahmet tattırır da sonra bunu ondan çekip alırsak, şüphesiz ki o tamamen ümitsiz, nankör olur. Kendisine dokunan bir zarardan sonra ona bir nimet tattırırsak, elbette \"Kötülükler benden gitti.\" der. Şüphesiz ki o, çok şımarıktır, çok kibirlidir. Ancak sabredip güzel işler yapanlar başka. İşte onlar için bağışlanma ve büyük bir ödül vardır.",
+		author: "Kur'an, 11:9-11"
+	},
+	{
+		text: "Hayır! Şüphesiz ki insan, kendini zengin (ihtiyaçsız) gördüğü için azar.",
+		author: "Kur'an, 96:6-7"
+	},
+	{
+		text: "Aldatıp oyaladı o çokluk yarışı sizleri.",
+		author: "Kur'an, 102:1"
+	},
+]
